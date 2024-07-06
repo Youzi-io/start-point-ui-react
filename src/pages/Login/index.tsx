a476e3b2cdcede389cd671d206e1013bb74d577d
yuzu <1581523609@qq.com>
2024-07-06 22:39:24 +0800

    refactor:修改登录页面

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,19 +1,62 @@
+import { LoginParams } from "@/types/auth";
+import { Button, Form, Input } from "antd";
+import { ValidateErrorEntity } from "rc-field-form/lib/interface";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
-  console.log("登录页的组件");
+
+  const onFinish = (values: LoginParams) => {
+    console.log("Received values of form: ", values);
+    navigate("/");
+  };
+
+  const onFinishFailed = (errorInfo: ValidateErrorEntity<LoginParams>) => {
+    console.log("Failed:", errorInfo);
+  };
 
   return (
     <>
-      <div className="text-3xl font-bold">这是登录页</div>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        去仪表盘
-      </button>
+        <Form.Item<LoginParams>
+          label="账号"
+          name="account"
+          rules={[{ required: true, message: "Please input your account!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<LoginParams>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        {/* <Form.Item<LoginParams>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item> */}
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </>
   );
 };
