f2cd8e75da7e76ed539ca8964c6453b34b101132
Yuzu <1581523609@qq.com>
2024-07-13 13:49:19 +0800

    refactor：完善登录页

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,39 @@
 import { LoginParams } from "@/types/auth";
-import { Button, Form, Input } from "antd";
+import { Button, Card, Checkbox, Form, Input } from "antd";
 import { ValidateErrorEntity } from "rc-field-form/lib/interface";
 import { useNavigate } from "react-router-dom";
+import styles from "./index.module.scss";
+import MSIcon from "@/components/MSIcon";
+import { useState } from "react";
+import { CheckboxChangeEvent } from "antd/es/checkbox";
 
 const Login = () => {
+  return (
+    <div className={`${styles["login"]}`}>
+      <div className={`${styles["g-bg"]}`}>
+        <div
+          className={`${styles["g-polygon"]} ${styles["g-polygon-1"]}`}
+        ></div>
+        <div
+          className={`${styles["g-polygon"]} ${styles["g-polygon-2"]}`}
+        ></div>
+        <div
+          className={`${styles["g-polygon"]} ${styles["g-polygon-3"]}`}
+        ></div>
+      </div>
+      <FormCard />
+    </div>
+  );
+};
+
+const FormCard = () => {
   const navigate = useNavigate();
+  const [imgCode, setImgCode] = useState<string>("");
+  const [remember, setRemember] = useState<boolean>(false);
 
   const onFinish = (values: LoginParams) => {
     console.log("Received values of form: ", values);
+
     navigate("/");
   };
 
@@ -15,49 +41,102 @@ const Login = () => {
     console.log("Failed:", errorInfo);
   };
 
+  const getValidateCode = () => {
+    setImgCode("");
+    console.log("获取验证码");
+  };
+
+  const rememberChange = (e: CheckboxChangeEvent) => {
+    setRemember(e.target.checked);
+  };
+
   return (
-    <>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<LoginParams>
-          label="账号"
-          name="account"
-          rules={[{ required: true, message: "Please input your account!" }]}
+    <div className={`${styles["form-card"]}`}>
+      <Card style={{ width: 400 }}>
+        <div className="text-[30px] font-medium">登录到</div>
+        <div className="text-[30px] mt-[7px]">Start Point</div>
+        <Form
+          name="basic"
+          style={{ margin: "25px 0 20px" }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
+          <Form.Item<LoginParams>
+            name="account"
+            rules={[{ required: true, message: "请输入账号！" }]}
+          >
+            <Input
+              size="large"
+              prefix={
+                <MSIcon
+                  name="Monitor"
+                  style={{
+                    fontSize: 15,
+                  }}
+                />
+              }
+            />
+          </Form.Item>
 
-        <Form.Item<LoginParams>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item<LoginParams>
+            name="password"
+            rules={[{ required: true, message: "请输入密码！" }]}
+          >
+            <Input.Password
+              size="large"
+              prefix={
+                <MSIcon
+                  name="Lock"
+                  style={{
+                    fontSize: 15,
+                  }}
+                />
+              }
+            />
+          </Form.Item>
 
-        {/* <Form.Item<LoginParams>
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item> */}
+          <Form.Item<LoginParams>
+            name="captcha"
+            rules={[{ required: true, message: "请输入验证码！" }]}
+          >
+            <Input
+              size="large"
+              prefix={
+                <MSIcon
+                  name="Key"
+                  style={{
+                    fontSize: 15,
+                  }}
+                />
+              }
+              addonAfter={
+                <img
+                  className="cursor-pointer w-[100%] h-[100%]"
+                  src={imgCode}
+                  onClick={getValidateCode}
+                  alt="加载失败。"
+                />
+              }
+              className={`${styles["captcha-input"]}`}
+            />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
+          <Form.Item name="remember">
+            <Checkbox checked={remember} onChange={rememberChange}>
+              记住密码
+            </Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 };
 
