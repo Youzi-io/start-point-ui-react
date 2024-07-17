c99f0a4cd99ebab109804b43e1dcb8fa76237128
Yuzu <1581523609@qq.com>
2024-07-17 14:14:33 +0800

    feat：新增获取token方法

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -2,12 +2,14 @@ import { create } from "zustand";
 
 interface UserState {
   token: string;
+  getToken: () => string;
   setToken: (token: string) => void;
   clearToken: () => void;
 }
 
-const useUserStore = create<UserState>((set) => ({
+const useUserStore = create<UserState>((set, get) => ({
   token: "",
+  getToken: () => get().token,
   setToken: (token) => set({ token }),
   clearToken: () => set({ token: "" }),
 }));
